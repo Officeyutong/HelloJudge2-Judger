7ce757e2b5e389a4407c6b590e3566fa6c4166c4
officeyutong <officeyutong@163.com>
2019-05-14 21:37:09 +0800

    优化了收集数据的方式

--- runner.py
+++ runner.py
@@ -73,7 +73,8 @@ class DockerRunner:
         self.container.reload()
         if self.container.status != "exited":
             self.container.kill()
-        self.container.wait()
+        # self.container.wait()
+        self.container.reload()
         output = self.container.logs().decode()
         attr = self.container.attrs.copy()
         self.container.remove()
