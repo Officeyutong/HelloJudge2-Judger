8042b569566ea7e04c9d6a1ee4bb65785532eb37
officeyutong <i@ytbolg.net>
2019-08-30 13:46:25 +0800

    ?

--- watcher/docker_watcher.cpp
+++ watcher/docker_watcher.cpp
@@ -47,35 +47,30 @@ python::tuple watch(int pid, int time_limit) {
                         .str();
         }
     }
+    fclose(fp);
     auto begin = get_current_usec();
     int64_t memory_result = -1, time_result = -1;
     const char* memory_file = memory.c_str();
-    cout << "before loop" << endl;
     while (!kill(pid, 0)) {
         if (memory_result == -1) {
             auto fp = fopen(memory_file, "r");
             if (!fp) continue;
             int64_t curr;
             // fseek()
-            cout << "setting memory" << endl;
             if (fscanf(fp, "%" SCNd64, &curr) > 0) {
                 memory_result = curr;
-                cout << "read ok" << endl;
             }
-
             fclose(fp);
-            cout << "set memory ok" << endl;
         }
-        usleep(1000);
+        usleep(100);
         time_result = get_current_usec() - begin;
         if (time_result >= time_limit * 1000) {
             break;
         }
     }
-    cout << "done" << endl;
     if (memory_result == -1) memory_result = 0;
     if (time_result == -1) time_result = 0;
-    fclose(fp);
+
     return python::make_tuple(time_result / 1000, memory_result);
 }
 
